e44321408a7a4f0663c8fcacba7bb6c995bc21ae
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2023-08-18 16:55:00 +0530

    portfolio

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <article className="footer">
+      <a
+        href="https://github.com/Anand7007"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Designed & Built by Anand &copy;2023
+      </a>
+    </article>
+  );
+};
+
+export default Footer;
---
+++ src/routes/about/Stack.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Stack = () => {
+  return (
+    <React.Fragment>
+      <h3 className="title-font white-text ">
+        Here are a few tools & technologies I’ve been working with:
+      </h3>
+      <ol className="stack-section ">
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>Node.Js</li>
+        <li>SASS</li>
+        <li>JavaScript ES6+</li>
+        <li>React.js</li>
+        <li>RESTful API</li>
+        <li>MongoDB</li>
+        <li>Git</li>
+        <li>Express</li>
+        <li>Visual Studio Code</li>
+      </ol>
+    </React.Fragment>
+  );
+};
+export default Stack;
